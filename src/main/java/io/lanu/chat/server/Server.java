77ce84d34ee9f86487d142473984561be3ec2f01
mrlanu <mrlanu@gmail.com>
2019-09-06 22:43:59 -0500

    inner class Handler & Server main method

--- src/main/java/io/lanu/chat/server/Server.java
+++ src/main/java/io/lanu/chat/server/Server.java
@@ -1,7 +1,32 @@
 package io.lanu.chat.server;
 
+import io.lanu.chat.util.ConsoleHelper;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Enter Server's port...");
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            ConsoleHelper.writeMessage("Server has been started on port - " + port);
+            while (true){
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Error occurs, Server has been stopped");
+        }
+    }
 
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
