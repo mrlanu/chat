d2c6e764d17ccee2579097adf29256f422ad9e1c
mrlanu <mrlanu@gmail.com>
2019-09-06 22:51:41 -0500

    sendBroadcastMessage method

--- src/main/java/io/lanu/chat/server/Server.java
+++ src/main/java/io/lanu/chat/server/Server.java
@@ -1,12 +1,27 @@
 package io.lanu.chat.server;
 
 import io.lanu.chat.util.ConsoleHelper;
+import io.lanu.chat.util.Message;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public void sendBroadcastMessage(Message message){
+        connectionMap.forEach((k, v) -> {
+            try {
+                v.sendMessage(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Error occurs while sending the message...");
+            }
+        });
+    }
+
     public static void main(String[] args) {
         ConsoleHelper.writeMessage("Enter Server's port...");
         int port = ConsoleHelper.readInt();
