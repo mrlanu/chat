7d43f583ad13b6bde1d521313ef05591ebb32278
mrlanu <mrlanu@gmail.com>
2019-09-07 21:47:24 -0500

    class Client

---
+++ src/main/java/io/lanu/chat/client/Client.java
@@ -0,0 +1,12 @@
+package io.lanu.chat.client;
+
+import io.lanu.chat.server.Connection;
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public class SocketThread extends Thread{
+
+    }
+}
