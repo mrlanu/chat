59950d002fdcdc310c854b61f9ab47fc85722da6
mrlanu <mrlanu@gmail.com>
2019-09-07 21:56:18 -0500

    helped methods added to Client

--- src/main/java/io/lanu/chat/client/Client.java
+++ src/main/java/io/lanu/chat/client/Client.java
@@ -1,10 +1,47 @@
 package io.lanu.chat.client;
 
 import io.lanu.chat.server.Connection;
+import io.lanu.chat.util.ConsoleHelper;
+import io.lanu.chat.util.Message;
+import io.lanu.chat.util.MessageType;
+
+import java.io.IOException;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Enter a server's address, please");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Enter a server's port, please");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Enter a nickname, please");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSentTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.sendMessage(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Errors occurs...");
+            clientConnected = false;
+        }
+    }
 
     public class SocketThread extends Thread{
 
